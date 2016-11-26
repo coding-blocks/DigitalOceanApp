1dd171ded9c430e9e9c3c7bc60a1cbac9b740541
Jonsnow21 <neeraj14.nj@gmail.com>
2016-11-26 19:07:27 +0530

    create layout activity detail droplet

--- app/src/main/java/in/tosc/digitaloceanapp/DetailDropletActivity.java
+++ app/src/main/java/in/tosc/digitaloceanapp/DetailDropletActivity.java
@@ -3,19 +3,31 @@ package in.tosc.digitaloceanapp;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 
 /**
  * Created by Jonsnow21 on 26/11/16.
  */
 
-public class DetailDropletActivity extends AppCompatActivity{
+public class DetailDropletActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+
+    private SwitchCompat switchIPv6, switchPrivateNet, switchBackup;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_detail_droplet);
+
+        switchIPv6 = (SwitchCompat) findViewById(R.id.switch_ipv6);
+        switchPrivateNet = (SwitchCompat) findViewById(R.id.switch_private_network);
+        switchBackup = (SwitchCompat) findViewById(R.id.switch_backup);
+
+        switchIPv6.setOnCheckedChangeListener(this);
+        switchPrivateNet.setOnCheckedChangeListener(this);
+        switchBackup.setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -38,5 +50,37 @@ public class DetailDropletActivity extends AppCompatActivity{
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.switch_ipv6:
+                if (!isChecked){
+
+                } else{
+
+                }
+                break;
+
+            case R.id.switch_private_network:
+                if (!isChecked) {
+
+                } else {
+
+                }
+                break;
+
+            case R.id.switch_backup:
+                if (!isChecked) {
+
+                } else {
+
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
