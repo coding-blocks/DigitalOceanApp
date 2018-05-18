a9cb8835c4e6962e62d57d4956f6ffacad1ccb40
AkshayCHD <akshaykumarsharma1998aks@gmail.com>
2018-05-18 13:39:24 +0530

    Add databinding to fragments (#74)

--- app/src/main/java/in/tosc/digitaloceanapp/fragments/DataCenterFragment.java
+++ app/src/main/java/in/tosc/digitaloceanapp/fragments/DataCenterFragment.java
@@ -1,17 +1,18 @@
 package in.tosc.digitaloceanapp.fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.databinding.DataBindingUtil;
+
 import in.tosc.digitaloceanapp.R;
 import in.tosc.digitaloceanapp.adapters.DataCenterAdapter;
+import in.tosc.digitaloceanapp.databinding.FragmentSelectDataCenterBinding;
 import in.tosc.doandroidlib.DigitalOcean;
 import in.tosc.doandroidlib.api.DigitalOceanClient;
 import in.tosc.doandroidlib.objects.Regions;
@@ -39,17 +40,18 @@ public class DataCenterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_select_data_center, container, false);
-        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.datacenter_recycler);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        final FragmentSelectDataCenterBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_select_data_center, container, false);
+
+        binding.datacenterRecycler.setLayoutManager(new GridLayoutManager(getActivity(), 2));
         DigitalOceanClient doClient = DigitalOcean.getDOClient(getContext().getSharedPreferences("DO", MODE_PRIVATE).getString("authToken", null));
-        recyclerView.setAdapter(dataCenterAdapter);
+        binding.datacenterRecycler.setAdapter(dataCenterAdapter);
         doClient.getRegions().enqueue(new Callback<Regions>() {
             @Override
             public void onResponse(Call<Regions> call, Response<Regions> response) {
                 Regions regions = response.body();
                 DataCenterAdapter dataCenterAdapter = new DataCenterAdapter(regions, getActivity());
-                recyclerView.setAdapter(dataCenterAdapter);
+                binding.datacenterRecycler.setAdapter(dataCenterAdapter);
                 dataCenterAdapter.notifyDataSetChanged();
                 Log.i(TAG, "onResponse: " + regions.getRegions().size() + " Regions Fetched Successfully!");
             }
@@ -60,7 +62,7 @@ public class DataCenterFragment extends Fragment {
             }
         });
 
-        return view;
+        return binding.getRoot();
     }
 
 
--- app/src/main/java/in/tosc/digitaloceanapp/fragments/SelectImageFragment.java
+++ app/src/main/java/in/tosc/digitaloceanapp/fragments/SelectImageFragment.java
@@ -3,16 +3,17 @@ package in.tosc.digitaloceanapp.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.databinding.DataBindingUtil;
 import java.util.List;
 
 import in.tosc.digitaloceanapp.R;
 import in.tosc.digitaloceanapp.adapters.ImageAdapter;
+import in.tosc.digitaloceanapp.databinding.FragmentImageBinding;
 import in.tosc.doandroidlib.DigitalOcean;
 import in.tosc.doandroidlib.api.DigitalOceanClient;
 import in.tosc.doandroidlib.objects.Image;
@@ -27,7 +28,6 @@ public class SelectImageFragment extends Fragment {
 
     public static List<Image> imageList;
     ImageAdapter imageAdapter;
-    RecyclerView imageRecyclerView;
 
     public SelectImageFragment() {
 
@@ -43,18 +43,19 @@ public class SelectImageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_image, container, false);
+        final FragmentImageBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_image, container, false);
+
         DigitalOceanClient doClient = DigitalOcean.getDOClient(getContext().getSharedPreferences("DO", MODE_PRIVATE).getString("authToken", null));
-        imageRecyclerView = (RecyclerView) view.findViewById(R.id.imageRecyclerVIew);
-        imageRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
-        imageRecyclerView.setAdapter(imageAdapter);
+        binding.imageRecyclerVIew.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        binding.imageRecyclerVIew.setAdapter(imageAdapter);
 
         doClient.getImages(1, 100, "distribution").enqueue(new Callback<Images>() {
             @Override
             public void onResponse(Call<Images> call, Response<Images> response) {
                 imageList = response.body().getImages();
                 imageAdapter = new ImageAdapter(imageList, getContext());
-                imageRecyclerView.setAdapter(imageAdapter);
+                binding.imageRecyclerVIew.setAdapter(imageAdapter);
                 Log.e("Droplets fetched", String.valueOf(imageList.size()));
             }
 
@@ -63,6 +64,7 @@ public class SelectImageFragment extends Fragment {
                 Log.e("Failed getting images", t.getLocalizedMessage());
             }
         });
-        return view;
+
+        return binding.getRoot();
     }
 }
