64cccca9152a26cfe371a2bff1ca8ee92613accc
aggarwalpulkit596 <app.mathonogo@gmail.com>
2018-05-20 18:50:41 +0530

    Removed test on oncreateoptionmenu on DropletActivity

--- app/src/androidTest/java/in/tosc/digitaloceanapp/activities/DropletActivityTest.java
+++ app/src/androidTest/java/in/tosc/digitaloceanapp/activities/DropletActivityTest.java
@@ -6,16 +6,12 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.click;
+
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import android.view.Gravity;
 import android.view.WindowManager;
