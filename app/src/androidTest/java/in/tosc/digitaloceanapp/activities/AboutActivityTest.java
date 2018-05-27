f0378b230e4a3799acb0a9cc71b7019ea35928c3
venomousboxer <y.mohit03031999@gmail.com>
2018-05-27 17:15:20 +0530

    solving more errors

--- app/src/androidTest/java/in/tosc/digitaloceanapp/activities/AboutActivityTest.java
+++ app/src/androidTest/java/in/tosc/digitaloceanapp/activities/AboutActivityTest.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
