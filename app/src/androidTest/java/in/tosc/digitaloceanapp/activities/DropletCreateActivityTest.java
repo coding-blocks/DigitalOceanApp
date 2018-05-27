42193b1b32fbb66e9072da6c4afd5dd673aea6af
venomousboxer <y.mohit03031999@gmail.com>
2018-05-27 16:47:40 +0530

    solving more errors

--- app/src/androidTest/java/in/tosc/digitaloceanapp/activities/DropletCreateActivityTest.java
+++ app/src/androidTest/java/in/tosc/digitaloceanapp/activities/DropletCreateActivityTest.java
@@ -1,8 +1,8 @@
 package in.tosc.digitaloceanapp.activities;
 
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 import android.support.test.rule.ActivityTestRule;
 import android.view.WindowManager;
